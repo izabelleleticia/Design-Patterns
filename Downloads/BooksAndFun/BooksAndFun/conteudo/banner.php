d8e2dfa89c1d610c01c599fc368181f33138b112
Izabelle Leticia <165915824+izabelleleticia@users.noreply.github.com>
2026-04-16 12:29:17 -0300

    Delete Downloads/BooksAndFun/BooksAndFun directory

--- Downloads/BooksAndFun/BooksAndFun/conteudo/banner.php
+++
@@ -1,9 +0,0 @@
-<section class="banner">
-           
-            <h1>Reserve o seu livro</h1>
-            <img src="img/livraria.img.png" alt="Banner 1">
-            <!-- <div>
-                <img src="img/1.png" alt="Logo da Empresa" class="logo">
-            </div> -->
-           
-        </section>
